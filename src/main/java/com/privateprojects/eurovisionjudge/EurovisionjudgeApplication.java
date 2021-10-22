7bafe94aa25bcb519a72fff781db2829aba74737
RobinFrancq <robin.francq@outlook.com>
2021-10-22 18:55:11 +0200

    Initial project commit

---
+++ src/main/java/com/privateprojects/eurovisionjudge/EurovisionjudgeApplication.java
@@ -0,0 +1,13 @@
+package com.privateprojects.eurovisionjudge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurovisionjudgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurovisionjudgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/privateprojects/eurovisionjudge/EurovisionjudgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.privateprojects.eurovisionjudge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurovisionjudgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
