a210a18c186af0c04a8233f3f4ac1ccf60c21a11
RobinFrancq <robin.francq@outlook.com>
2021-11-07 11:32:36 +0100

    linked login form to login api (work in progress)

--- src/main/java/com/privateprojects/eurovisionjudge/model/exception/responseException/EntityNotFoundException.java
+++ src/main/java/com/privateprojects/eurovisionjudge/model/exception/responseException/EntityNotFoundException.java
@@ -3,6 +3,6 @@ package com.privateprojects.eurovisionjudge.model.exception.responseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Could not find the entity")
 public class EntityNotFoundException extends RuntimeException {
 }
--- src/main/java/com/privateprojects/eurovisionjudge/model/exception/responseException/WrongCredentialsException.java
+++ src/main/java/com/privateprojects/eurovisionjudge/model/exception/responseException/WrongCredentialsException.java
@@ -3,6 +3,6 @@ package com.privateprojects.eurovisionjudge.model.exception.responseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.UNAUTHORIZED)
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED, reason = "Wrong credentials were used")
 public class WrongCredentialsException extends RuntimeException {
 }
